266b1875118534523a779230240e7b69ce94a2a5
Gwen Demulder <31825690+scara1701@users.noreply.github.com>
2020-02-15 14:24:23 +0100

    Add project files.

---
+++ IdentityTest/Models/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityTest.Models
+{
+    public class ApplicationRole
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string NormalizedName { get; set; }
+    }
+}
